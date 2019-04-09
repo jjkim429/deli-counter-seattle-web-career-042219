904533b1c23f893205a576e67fa9e991ed39b739
Jennifer Kim <jjkim429@gmail.com>
2019-04-09 03:28:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,11 @@ def line(array)
   else
   newarray = []
   counter = 0
-  array.each do |element|
-    newarray.push("#{array.index(counter) + 1}. ")
-    counter += 1
-  end
+    array.each do |element|
+      newarray.push("#{array.index(counter) + 1}. ")
+     counter += 1
+    end
+  
 end
 
 def take_a_number(array, customer)
