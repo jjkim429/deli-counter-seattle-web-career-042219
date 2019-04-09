88a5129830796426d1e4a9361001da568c57a409
Jennifer Kim <jjkim429@gmail.com>
2019-04-09 03:25:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  end
+  else
+  newarray = []
+  counter = 1
+  until counter = array.length
+end
+
+def take_a_number(array, customer)
+    array.push(customer)
+    place = array.index(customer)
+    puts "#{customer}, #{array.index(customer) + 1}"
+end
+
+def now_serving(array)
+  if array.empty? == true
+    puts "There is nobody waiting to be served!"
+  end
+end
+  
