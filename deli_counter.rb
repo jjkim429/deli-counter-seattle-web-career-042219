a37c18402fa56b116fe2fe674534972e873693be
Jennifer Kim <jjkim429@gmail.com>
2019-04-09 03:32:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,13 +3,13 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   end
-  else
-  newarray = []
-  counter = 0
-    array.each do |element|
-      newarray.push("#{array.index(counter) + 1}. ")
-     counter += 1
-    end
+  ##else
+  #newarray = []
+  #counter = 0
+  #  array.each do |element|
+  #    newarray.push("#{array.index(counter) + 1}. ")
+  #   counter += 1
+  #  end
 end
 
 def take_a_number(array, customer)
